40776e90a973eb6b00d16e62539f42d4219ca3fc
Quantum <quantum2048@gmail.com>
2013-05-31 22:46:30 -0400

    Zalgo'd the name.
    Signed-off-by: Xiaomao Chen <xiaomao5@live.com>

--- src/Zalgo.cpp
+++ src/Zalgo.cpp
@@ -1,4 +1,4 @@
-#include <MainWindow.hpp>
+﻿#include <MainWindow.hpp>
 #include <commctrl.h>
 #include <objbase.h>
 
@@ -17,7 +17,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     COleInitialize oleInit;
     InitCommonControls();
 
-    MainWindow *win = MainWindow::Create(L"Unicode Diacritic-Polluted Text Generator");
+    MainWindow *win = MainWindow::Create(L"U̬̩͔̐̿ṅ̨̤̬̽i̛̘̳̹ͤ̍c̠̳̬͛o̘̳͗̿̈́d̗̅̓͗͠ḛ͍͛ ̬̠̹̉ͭ͛D̨̤̰̀̂ͦì̛͇͂a͍͇͛ͨͤc̡̟͖͗̔ͤͅr͔͇ͮ̓̍͢i̴͇͇̇͋̽t̛̟̟͋i̲̘̿̊c̺͎ͬ͗-̴̤́̔P̋̍҉͎̹ö̲̯͈̋͞l͎͂l̻̖ͩ̂͜u̵͔̳̇̋t̳̪͐̎e͈̓͢ḓ̗̭̓ ̹̫͛Ṭͫe̙̝̦̊̊̑͢x̶͉ͦ̚t̞̔̈́̀ ̡̪̪̙͒͗G̘̜̋e͍̯̻͋ͬͦn̹̩̫̑̈́ẽ͔̳r̠͙͒̀̅ͅa̭ͫ̓́t̘̺̋̏̚o̰̙̦ͪṛͦͣ́ͅ");
     if (win) {
         ShowWindow(win->GetHWND(), nCmdShow);
         MSG msg;
